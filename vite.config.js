7e986d7100c1bfc0307cba4d22c4898596bfb948
sabitmemmedovh <sabitmemmedovh@gmail.com>
2025-11-21 16:02:21 +0400

    Prepare project for deployment: fix Vite base, add netlify.toml and .vercelignore

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
-  base: process.env.VITE_BASE_PATH  || "react-hotel-booking",
+  base: process.env.VITE_BASE_PATH || '/',
 })
